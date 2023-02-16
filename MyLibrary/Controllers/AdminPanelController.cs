c04f49bb597b035759ba6a18546e22ed6f075247
Jakub Feliński <80295@student.pb.edu.pl>
2023-02-16 15:53:45 +0100

    Admin panel, user roles management

---
+++ MyLibrary/Controllers/AdminPanelController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyLibrary.Controllers
+{
+    public class AdminPanelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult RoleManager()
+        {
+            return RedirectToAction("Index", "RoleManager");
+        }
+        
+        public IActionResult UserRoles()
+        {
+            return RedirectToAction("Index", "UserRoles");
+        }
+    }
+}
--- MyLibrary/Controllers/HomeController.cs
+++ MyLibrary/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace MyLibrary.Controllers
             return View();
         }
 
+        public IActionResult AdminPanel()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
